62cb4dbbef514a9b6dc9c2ef122fb724ef7105f6
MOUAD BEN MEKKI <73074210+BenMekkiMouad@users.noreply.github.com>
2022-06-27 01:51:56 +0100

    Create footer.blade.php

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,42 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+<script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+
+<footer>
+        <div class="footer-content container">
+            <div class="made-with">Made with <i class="bi bi-suit-heart-fill"></i> by Ben Mekki Mouad</div>
+            <ul>
+            <span><a href="https://www.linkedin.com/in/mouad-ben-mekki-b87219216"><i class="bi bi-linkedin"></i></a></span>
+            <span><a href="https://www.instagram.com/grandpaframe/"><i class="bi bi-instagram"></i></a></span>
+            <span><a href="mailto:benmekkimouad@gmail.com""><i class="fa fa-envelope"></i></a></span>
+            </ul>
+        </div> <!-- end footer-content -->
+    </footer>
+    <style>
+        footer {
+            background: #f0f8ff;
+            color: #779cb1;
+            padding: 40px 0;
+        }
+
+        .footer-content {
+            display: flex;
+            justify-content: space-between;
+
+            .bi bi-suit-heart-fill {
+            color: #ff748c;
+            }
+
+            ul {
+            display: flex;
+            width: 30%;
+            justify-content: space-between;
+            
+            }
+
+            a {
+            color: white;
+            
+            }
+           
+        }
+    </style>
